1c403153873302d2d1cb2a4e4fc68ed51384547c
Wvaviator <wvaviator@gmail.com>
2022-12-15 21:14:32 -0600

    implement code execution via judge0

--- components/PaintableCodeEditor/PaintableCodeEditor.tsx
+++ components/PaintableCodeEditor/PaintableCodeEditor.tsx
@@ -1,9 +1,10 @@
-import Editor from '@monaco-editor/react';
+import Editor, { Monaco } from '@monaco-editor/react';
 import React from 'react';
 import { PaintableComponentProps } from '../ComponentCanvas/ComponentCanvas';
 import PaintableDiv from '../PaintableDiv/PaintableDiv';
 import styles from './PaintableCodeEditor.module.css';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import { CircularProgress } from '@mui/material';
 
 interface PaintableCodeEditorProps extends PaintableComponentProps {}
 
@@ -13,12 +14,40 @@ const PaintableCodeEditor: React.FC<PaintableCodeEditorProps> = ({
   setData,
   ...rest
 }) => {
+  const [consoleOutput, setConsoleOutput] = React.useState<string[]>([]);
+  const [loadingConsoleOutput, setLoadingConsoleOutput] =
+    React.useState<boolean>(false);
+
+  const editorRef = React.useRef(null);
+
+  const handleEditorMount = (editor: any) => {
+    editorRef.current = editor;
+    const value = editor.getValue();
+    setData({ ...data, code: value });
+  };
+
   const handleEditorChange = (value: string | undefined) => {
     setData({ ...data, code: value });
   };
 
-  const handleCodeExecute = () => {
-    //TODO: Send code to backend for execution and handle console output
+  const handleCodeExecute = async () => {
+    setLoadingConsoleOutput(true);
+    const response = await fetch('/api/compile', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ code: data.code }),
+    });
+    const { stdout, success } = await response.json();
+    if (success) {
+      const newConsoleOutput: string[] = stdout.split('\n');
+      console.log('newConsoleOutput: ', newConsoleOutput);
+      setConsoleOutput(newConsoleOutput);
+    } else {
+      console.log('An error occurred.');
+    }
+    setLoadingConsoleOutput(false);
   };
 
   return (
@@ -46,10 +75,15 @@ const PaintableCodeEditor: React.FC<PaintableCodeEditorProps> = ({
                 ? `const add = (a, b) => {\n  return a + b;\n}\n\nconsole.log(add(1, 2));\n`
                 : data.code
             }
+            onMount={handleEditorMount}
             onChange={handleEditorChange}
           />
           <div className={styles.controls} onClick={handleCodeExecute}>
-            <PlayArrowIcon />
+            {loadingConsoleOutput ? (
+              <CircularProgress size="1.25rem" thickness={6} />
+            ) : (
+              <PlayArrowIcon />
+            )}
           </div>
         </div>
 
@@ -57,7 +91,9 @@ const PaintableCodeEditor: React.FC<PaintableCodeEditorProps> = ({
           className={styles.console}
           onPointerDown={(event) => event.stopPropagation()}
         >
-          <pre>test console output</pre>
+          {consoleOutput.map((line, index) => (
+            <pre key={index}>{line}</pre>
+          ))}
         </div>
       </div>
     </PaintableDiv>
