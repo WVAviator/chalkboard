df707f540682dd7b3b3b3d7705d2fd0a74811b46
Wvaviator <wvaviator@gmail.com>
2023-01-10 11:11:54 -0600

    update s3 env names

--- lib/s3.ts
+++ lib/s3.ts
@@ -2,9 +2,9 @@ import aws from 'aws-sdk';
 import crypto from 'crypto';
 
 const s3 = new aws.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  accessKeyId: process.env.CHALKBOARD_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.CHALKBOARD_AWS_SECRET_ACCESS_KEY,
+  region: process.env.CHALKBOARD_AWS_REGION,
   signatureVersion: 'v4',
 });
 
@@ -12,7 +12,7 @@ export const generateUploadURL = async () => {
   const randomKey = crypto.randomBytes(16).toString('hex');
 
   const params = {
-    Bucket: process.env.AWS_BUCKET,
+    Bucket: process.env.CHALKBOARD_AWS_BUCKET,
     Key: randomKey,
     Expires: 60,
   };
