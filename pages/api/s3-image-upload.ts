d72b655bd4a2bf893dd3c5a69fa2feb32936b50c
Wvaviator <wvaviator@gmail.com>
2023-01-15 14:27:12 -0600

    add server limiting of image upload url request

--- pages/api/s3-image-upload.ts
+++ pages/api/s3-image-upload.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import getSessionUser from '../../lib/getSessionUser';
 import { generateUploadURL } from '../../lib/s3';
 
 export default async function handler(
@@ -6,9 +7,30 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method !== 'GET') {
-    return res.status(403).json({ message: 'Method not allowed' });
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  const sessionUser = await getSessionUser(req);
+  if (!sessionUser) {
+    console.log('Session not found.');
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  if (
+    sessionUser.accountLimits.images.current >=
+    sessionUser.accountLimits.images.max
+  ) {
+    console.log('User has reached image limit.');
+    return res.status(403).json({
+      message: 'Unable to save, image limit reached.',
+      success: false,
+    });
   }
 
   const url = await generateUploadURL();
+
+  sessionUser.accountLimits.images.current += 1;
+  await sessionUser.save();
+
   res.status(200).send({ url });
 }
